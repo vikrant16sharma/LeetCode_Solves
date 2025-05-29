47b7a779ecbc081cad6790e60db676c535f7d194
Vikrant Sharma <s.vikrant3002@gmail.com>
2025-05-29 08:59:15 +0530

    hello world

---
+++ src/Arrays/TwoSum.java
@@ -0,0 +1,66 @@
+package Arrays;
+
+import java.util.Scanner;
+
+//1. Two Sum
+//        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+//        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//        You can return the answer in any order.
+//
+//
+//
+//        Example 1:
+//
+//        Input: nums = [2,7,11,15], target = 9
+//        Output: [0,1]
+//        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//        Example 2:
+//
+//        Input: nums = [3,2,4], target = 6
+//        Output: [1,2]
+//        Example 3:
+//
+//        Input: nums = [3,3], target = 6
+//        Output: [0,1]
+//
+//
+//        Constraints:
+//
+//        2 <= nums.length <= 104
+//        -109 <= nums[i] <= 109
+//        -109 <= target <= 109
+//        Only one valid answer exists.
+//
+//
+//        Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+public class TwoSum {
+    public static int[] twoSum(int[] nums,int target){
+//        Brute force approch
+        for(int i = 0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return null;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] nums = new int[len];
+        for(int i =0 ;i<nums.length;i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target: ");
+        int target = sc.nextInt();
+        int [] res = twoSum(nums,target);
+        for(int i = 0 ;i<res.length;i++){
+            System.out.print(res[i]);
+        }
+        System.out.println();
+    }
+}
---
+++ src/LinkedList/Basics.java
@@ -0,0 +1,85 @@
+package LinkedList;
+
+public class Basics {
+    public int size = 0;
+    public Node head;
+    public class Node{
+        public int val;
+        public Node next;
+        public Node(int val){
+            this.val = val;
+            this.next = null;
+        }
+        public Node(int val,Node next){
+            this.next = next;
+            this.val = val;
+        }
+    }
+    public Node insertFirst(int val){
+        Node newNode = new Node(val);
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+
+        }
+        size++;
+        return head;
+    }
+    public void insertLast(Node head,int val){
+        Node newNode = new Node(val);
+        if(head==null){
+            insertFirst(val);
+        }
+        Node temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    public void display(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+"->");
+            temp = temp.next;
+        }
+        System.out.print("Null");
+    }
+    public void insertPosition(Node head,int val,int pos){
+        if(pos<0 || pos>size){
+            System.out.println("it can't ouccur in this senerio.");
+        }
+        Node newNode = new Node(val);
+        if(head == null){
+            insertFirst(val);
+        }
+        Node temp = head;
+        for(int i = 0;i<pos-1;i++){
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+    public static void main(String[] args) {
+//        we will perform some basics insertions and deletions on the linked list to getbetter idea of basics
+        Basics obj = new Basics();
+        obj.insertFirst(3);
+        obj.insertFirst(2);
+        obj.insertFirst(1);
+        obj.display(obj.head);
+        System.out.println();
+        obj.insertLast(obj.head, 5);
+        obj.insertLast(obj.head, 6);
+        obj.display(obj.head);
+        System.out.println();
+        obj.insertPosition(obj.head,4,3);
+        obj.display(obj.head);
+    }
+}
+
+
+
+
